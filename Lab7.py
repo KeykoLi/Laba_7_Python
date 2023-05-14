b3876658eee849555492fb236503d515e267f9cd
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2023-05-14 18:03:26 +0300

    make parameters private

--- Lab7.py
+++ Lab7.py
@@ -1,33 +1,33 @@
 
 class Stadium:
     def __init__(self, name="empty", capacity=0, current_attendance=0, home_team="empty", away_team="empty"):
-        self.name = name
-        self.capacity = capacity
-        self.current_attendance = current_attendance
-        self.home_team = home_team
-        self.away_team = away_team
+        self._name = name
+        self._capacity = capacity
+        self._current_attendance = current_attendance
+        self._home_team = home_team
+        self._away_team = away_team
 
     instance = None
 
     def add_attendance(self, count):
-        if self.capacity >= self.current_attendance + count:
-            self.current_attendance += count
+        if self._capacity >= self._current_attendance + count:
+            self._current_attendance += count
 
     def decrease_attendance(self):
         DECREASE = 100
-        if self.current_attendance > DECREASE:
-            self.current_attendance -= DECREASE
+        if self._current_attendance > DECREASE:
+            self._urrent_attendance -= DECREASE
 
     def change_home_team(self, team_name):
-        self.home_team = team_name
+        self._home_team = team_name
 
     def change_away_team(self, team_name):
-        self.away_team = team_name
+        self._away_team = team_name
 
     def to_string(self):
-        return f"Stadium: name = {self.name}, capacity = {self.capacity}, " \
-               f"current attendance =  {self.current_attendance}, home team = {self.home_team}, " \
-               f"away team = {self.away_team}"
+        return f"Stadium: name = {self._name}, capacity = {self._capacity}, " \
+               f"current attendance =  {self._current_attendance}, home team = {self._home_team}, " \
+               f"away team = {self._away_team}"
 
     @staticmethod
     def get_instance():
