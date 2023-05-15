76560ad2f1dd4308997717f76749a00e6bdc58c2
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2023-05-15 17:58:58 +0300

    parameters are made public again, except for "instance" + created module for constants

--- Lab7.py
+++ Lab7.py
@@ -1,43 +1,43 @@
+import constant
+
 
 class Stadium:
     def __init__(self, name="empty", capacity=0, current_attendance=0, home_team="empty", away_team="empty"):
-        self._name = name
-        self._capacity = capacity
-        self._current_attendance = current_attendance
-        self._home_team = home_team
-        self._away_team = away_team
+        self.name = name
+        self.capacity = capacity
+        self.current_attendance = current_attendance
+        self.home_team = home_team
+        self.away_team = away_team
 
-    instance = None
+    __instance = None
 
     def add_attendance(self, count):
-        if self._capacity >= self._current_attendance + count:
-            self._current_attendance += count
+        if self.capacity >= self.current_attendance + count:
+            self.current_attendance += count
 
     def decrease_attendance(self):
-        DECREASE = 100
-        if self._current_attendance > DECREASE:
-            self._urrent_attendance -= DECREASE
+        if self.current_attendance > constant.DECREASE:
+            self.current_attendance -= constant.DECREASE
 
     def change_home_team(self, team_name):
-        self._home_team = team_name
+        self.home_team = team_name
 
     def change_away_team(self, team_name):
-        self._away_team = team_name
+        self.away_team = team_name
 
-    def to_string(self):
-        return f"Stadium: name = {self._name}, capacity = {self._capacity}, " \
-               f"current attendance =  {self._current_attendance}, home team = {self._home_team}, " \
-               f"away team = {self._away_team}"
+    def _str_(self):
+        return f"Stadium: name = {self.name}, capacity = {self.capacity}, " \
+               f"current attendance =  {self.current_attendance}, home team = {self.home_team}, " \
+               f"away team = {self.away_team}"
 
     @staticmethod
     def get_instance():
-        if Stadium.instance is None:
-            Stadium.instance = Stadium()
-        return Stadium.instance
+        if Stadium.__instance is None:
+            Stadium.__instance = Stadium()
+        return Stadium.__instance
 
 
 if __name__ == "__main__":
-    default_stadium = Stadium()
     stadiums = [
         Stadium(),
         Stadium("Spartac", 3000, 2140, "Dinamo", "Shahtar"),
@@ -45,4 +45,6 @@ if __name__ == "__main__":
         Stadium.get_instance()
     ]
 for stadium in stadiums:
-    print(stadium.to_string())
+    print(stadium._str_())
+
+
